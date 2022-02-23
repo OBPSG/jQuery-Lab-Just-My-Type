5bf7a380c1d619070d2e19b0a4fb509018ed0dcf
OBPSG <vbeasupergamer@gmail.com>
2022-02-23 14:39:46 -0600

    Added logic for ending the test, and for calculating and displaying the results

--- app.js
+++ app.js
@@ -13,11 +13,15 @@ $(function () {
     ];
     let check = "✔️"
     let X = "❌"
+    let $feedbackLine = $("#feedback");
     let sentencePosition = 0;
     let $prompt = $("#sentence");
     let sentence = sentenceArr[0];
     let sentencesIndex = 0;
     let expectedChar = sentence.charAt(0);
+    let timeStart, timeEnd;
+    let hasBegun = false;
+    let errorCount = 0;
     let $target = $("#target-letter");
     $target.text(expectedChar);
 
@@ -32,6 +36,12 @@ $(function () {
             $lowerCaseKeyboard.hide();
             $upperCaseKeyboard.show();
         } else {
+            //If this is the first key that's been typed, begin the timer
+            if(!hasBegun)
+            {
+                hasBegun = true;
+                timeStart = Date.now();
+            }
             sentencePosition += 1;
             //Highlight the current key that was pressed on the keyboard map
             let keyID = event.key.charCodeAt(0);
@@ -39,7 +49,16 @@ $(function () {
             $keyPressed.addClass("key-highlight");
 
             if (sentencePosition < sentence.length) {
-                //TODO: Check if the input character is equal to the expected one, and update the feedback accordingly
+                //Check if the input character is equal to the expected one, and update the feedback accordingly
+                if(event.key == expectedChar)
+                {
+                    $feedbackLine.append(check);
+                }
+                else
+                {
+                    $feedbackLine.append(X);
+                    errorCount += 1;
+                }
 
                 expectedChar = sentence.charAt(sentencePosition);
                 $target.text(expectedChar);
@@ -55,7 +74,9 @@ $(function () {
                     sentencesIndex += 1;
                     sentence = sentenceArr[sentencesIndex];
                     sentencePosition = 0;
-                    //TODO: clear the feedback div
+                    expectedChar = sentence[sentencePosition];
+                    //clear the feedback div
+                    $feedbackLine.empty();
                     //Display the next sentence
                     $prompt.empty();
                     let $highlightSpan = $("<span>" + sentence.charAt(sentencePosition) + "</span>");
@@ -63,11 +84,36 @@ $(function () {
                     //console.log($highlightSpan);
                     $prompt.append(sentence.substring(0, sentencePosition), $highlightSpan, sentence.substring(sentencePosition + 1, sentence.length));
                 }
+                else{
+                    //Finalizie the test
+                    timeEnd = Date.now();
+                    //Disable keyboard input by removing the event listeners for keydown and keyup
+                    $(document).off("keydown");
+                    $(document).off("keyup");
 
+                    $prompt.empty();
+                    $feedbackLine.empty();
+                    $target.empty();
+                    let elapsedTimeSeconds = Math.floor((timeEnd - timeStart) / 1000);
+                    //console.log("Elapsed Time: " + elapsedTimeSeconds + " seconds");
+
+                    //Display the Results
+                    const NUM_WORDS = 54;
+                    $prompt.text = "Results:"
+                    
+                    let elapsedTimeMinutes = Math.floor(elapsedTimeSeconds / 60);
+                    let timeSeconds = elapsedTimeSeconds % 60;
+
+                    let wordsPerMinute = NUM_WORDS * 60 / elapsedTimeSeconds - 2 * errorCount;
+
+                    $feedbackLine.append("Time: " + elapsedTimeMinutes + "m, " + timeSeconds + "s  |  ");
+                    $feedbackLine.append("Mistakes: " + errorCount + "  |  ");
+                    $feedbackLine.append("Words Per Minute: " + wordsPerMinute);
+                }
             }
-
         }
     });
+    
     $(document).on("keyup", function (event) {
         if (event.key === "Shift") {
             $lowerCaseKeyboard.show();
